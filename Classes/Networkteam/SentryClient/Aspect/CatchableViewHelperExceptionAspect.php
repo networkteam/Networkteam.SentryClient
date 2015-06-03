6e558f85950fd129ffe87ba67c6b0f613f4ada85
Dominik Piekarski <dominik.piekarski@networkteam.com>
2015-06-03 16:09:28 +0200

    Teach client to handle Exceptions from ViewHelpers
    This change introduces an aspect that listens on the render() method of the AbstractViewHelper for Exceptions and passes it to the sentry ErrorHandler.

---
+++ Classes/Networkteam/SentryClient/Aspect/CatchableViewHelperExceptionAspect.php
@@ -0,0 +1,29 @@
+<?php
+namespace Networkteam\SentryClient\Aspect;
+
+/***************************************************************
+ *  (c) 2015 networkteam GmbH - all rights reserved
+ ***************************************************************/
+
+use TYPO3\Flow\Annotations as Flow;
+
+/**
+ * @Flow\Aspect
+ */
+class CatchableViewHelperExceptionAspect {
+
+	/**
+	 * @Flow\Inject
+	 * @var \Networkteam\SentryClient\ErrorHandler
+	 */
+	protected $errorHandler;
+
+	/**
+	 * @Flow\AfterThrowing("within(TYPO3\Fluid\Core\ViewHelper\AbstractViewHelper) && method(.*->render())")
+	 * @param \TYPO3\Flow\Aop\JoinPoint $joinPoint
+	 */
+	public function catchException(\TYPO3\Flow\Aop\JoinPoint $joinPoint) {
+		$exception = $joinPoint->getException();
+		$this->errorHandler->handleException($exception);
+	}
+}
